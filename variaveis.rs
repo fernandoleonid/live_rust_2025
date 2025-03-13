c700e24df19517884493382c7103bb900b9fcc79
professor Fernando <fernandoleonid@gmail.com>
2025-03-13 20:38:30 -0300

    variaveis

---
+++ variaveis.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut idade = 50;
+    let preco: f64 = 99.99;
+    // let pago: bool = true;
+    // let nome: &str = "Fernando";
+    idade = 60;
+    println!("Sua idade é: {} e você pagou {}", idade, preco);
+}
