3d1173197802538510cb5f03faad84a3b5456e35
professor Fernando <fernandoleonid@gmail.com>
2025-03-27 17:29:35 -0300

    teste

--- live03/previsao_vendas/src/main.rs
+++ live03/previsao_vendas/src/main.rs
@@ -1,3 +1,22 @@
+struct RegistroVenda {
+    mes: f64,
+    valor: f64,
+}
+
+fn calcular_media(valores: &[f64]) -> f64 {
+    let soma: f64 = valores.iter().sum::<f64>();
+    let media: f64 = soma / valores.len() as f64;
+    media
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test = RegistroVenda  {
+        mes: 4.0,
+        valor: 100.99,
+    };
+
+    let valores = vec![1.0, 2.0, 3.0];
+    let media = calcular_media(&valores);
+    println!("Média: {}", media);    
+    println!("Mes: {} venda: {}", test.mes, test.valor);
 }
