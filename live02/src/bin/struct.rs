900fcb17806461c2c0c02cf704a5f953bd9ca3c0
professor Fernando <fernandoleonid@gmail.com>
2025-03-31 20:50:13 -0300

    live 02

--- live02/src/bin/struct.rs
+++ live02/src/bin/struct.rs
@@ -15,8 +15,6 @@ impl Produto {
     }
 }
 
-item1.mostrar_info();
-item2.mostrar_info();
 fn main () {
     let mut item1 = Produto::novo(String::from("Teclado"), 100.00);
     let mut item2 = Produto::novo(String::from("Mouse"), 88.99);
